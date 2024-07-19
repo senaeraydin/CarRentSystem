01ae148cfcf5d781aae72a738b0eb90c382c8189
senaeraydin <senaeraydin2@gmail.com>
2024-07-19 18:53:20 +0200

    committed
    committed

---
+++ Rento/Rento/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rento.Models;
+using Rento.Data;
+namespace Rento.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RentoContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new RentoContext();
+        }
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Car_List()
+        {
+            List<Car> carList = _dbContext.Cars.ToList();
+            return View(carList);
+        }
+      
+        public ActionResult Car_Info(int id)
+        {
+            var car = _dbContext.Cars.Find(id);
+            return View(car);
+        }
+       
+    }
+}
---
+++ Rento/Rento/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rento.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+
+        public String Maker { get; set; }
+
+        public String Model { get; set; }
+
+        public int Year { get; set; }
+
+        public Type CType { get; set; }
+
+        public String CImage { get; set; }
+
+        public float Price { get; set; }
+
+        public bool CAvailable { get; set; }
+    }
+
+    public enum Type
+    {
+        Normal,
+        Family, 
+        Sport
+    }
+}
